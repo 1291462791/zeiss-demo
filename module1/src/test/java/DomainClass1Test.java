004154f7641f484026150f8160795bc46d7707a5
song.wang <song.wang@thoughtworks.com>
2023-03-03 15:33:58 +0800

    first commit

---
+++ module1/src/test/java/DomainClass1Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertEquals;
+
+public class DomainClass1Test {
+    @Test
+    public void testUnitTestOne() {
+        DomainClass1 domainClass1 = new DomainClass1();
+        assertEquals( 15131, domainClass1.convert( 123));
+    }
+}
